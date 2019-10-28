c6f418a1c365c6cc412360f132e4b594e9813d7d
seungdam(home com) <erfrr@naver.com>
2019-10-28 23:18:38 +0900

    10.27 새벽~10.28
    충돌체크 최종적용 및 특수능력 구현

--- 20181820192DGP/chip/for_Idle.py
+++ 20181820192DGP/chip/for_Idle.py
@@ -1,16 +1,17 @@
 from pico2d import *
-import game_framework
+
 import pygame
 
-name = 'Stage1State'
 
+game = True
 player = None
-objectA = None
 block = []
 crushBlockList = []
+objectList = []
 count = 0
 backGround = None
 
+open_canvas(1280,640)
 tile_type = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 0
              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],  # 1
              [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0],  # 2
@@ -22,75 +23,6 @@ tile_type = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 0
              [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0],  # 8
              [0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 4, 0, 0]  # 9
              ]
-
-
-def enter():
-    global player, objectA, count, backGround
-    player = Player((500, 500))
-    objectA = Object((500, 100))
-    backGround = load_image('chip\\realBackGround.png')
-    for i in range(0, 10):
-        for k in range(0, 19):
-            if tile_type[i][k] == 1:
-                block.append(DownBlock((32 + 64 * k, 608 - i * 64)))
-                count += 1
-            elif tile_type[i][k] == 2:
-                block.append(LeftBlock((32 + 64 * k, 608 - i * 64)))
-                count += 1
-            elif tile_type[i][k] == 3:
-                crushBlockList.append(CrushBlock((32 + 64 * k, 608 - i * 64)))
-            elif tile_type[i][k] == 4:
-                block.append(RightBlock((32 + 64 * k, 608 - i * 64)))
-                count += 1
-    pass
-
-
-def exit():
-    global player, block
-    del player
-    del block
-    pass
-
-
-def resume():
-    pass
-
-
-def pause():
-    pass
-
-
-def handle_events():
-    global player
-    events = get_events()
-
-    for event in events:
-        if event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT and not player.falling:
-                player.state = 1
-            elif event.key == SDLK_LEFT and not player.falling:
-                player.state = -1
-            elif event.key == SDLK_UP and not player.falling:
-                player.state = 2
-            elif event.key == SDLK_DOWN and not player.falling:
-                player.state = -2
-            elif event.key == SDLK_x and not player.falling:
-                player.state = 3
-            elif event.key == SDLK_z and not player.falling:
-                player.state = -3
-            elif event.key == SDLK_ESCAPE:
-                game_framework.quit()
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_UP or event.key == SDLK_DOWN:
-                player.state = 0
-                pass
-            else:
-                player.state = 0
-        elif event.type == SDL_QUIT:
-            game_framework.quit
-    pass
-
-
 class Player:
     def __init__(self, pos):
         self.x = pos[0]
@@ -99,12 +31,14 @@ class Player:
         self.frame = 0
         self.speed = 4
         self.fallSpeed = 5
-        self.idle = load_image('chip\\character_idle.png')
-        self.run_right = load_image('chip\\character_run_right.png')
-        self.run_left = load_image('chip\\character_run_left.png')
-        self.up_down = load_image('chip\\character_updown2.png')
-        self.action_right = load_image('chip\\character_action_right.png')
-        self.action_left = load_image('chip\\character_action_left.png')
+
+        self.idle = load_image('character_idle.png')
+        self.run_right = load_image('character_run_right.png')
+        self.run_left = load_image('character_run_left.png')
+        self.up_down = load_image('character_updown2.png')
+        self.action_right = load_image('character_action_right.png')
+        self.action_left = load_image('character_action_left.png')
+
         self.falling = True
         self.left = self.x - 22
         self.top = self.y + 20
@@ -173,12 +107,7 @@ class Player:
         elif self.state == -3:
             self.action_left.clip_draw(self.frame * 64, 0, 64, 64, self.x, self.y)
 
-    def collision(self):
-        pass
-
-
 # -------------------------------------------------------------------------------
-
 
 class DownBlock:
     global player
@@ -186,7 +115,7 @@ class DownBlock:
 
     def __init__(self, pos):
         if DownBlock.image is None:
-            self.image = load_image('chip\\block4.png')
+            self.image = load_image('block4.png')
         self.x = pos[0]
         self.y = pos[1]
         self.left = self.x - 32
@@ -196,6 +125,8 @@ class DownBlock:
 
         self.drawing = True
         pass
+
+
 
     def check_collision(self):
         if player.left < self.right and player.right > self.left and player.bottom <= self.top and player.top >= self.bottom and player.falling:
@@ -222,7 +153,7 @@ class LeftBlock:
     global player
 
     def __init__(self,pos):
-        self.image = load_image('chip\\block3.png')
+        self.image = load_image('block3.png')
         self.x = pos[0]
         self.y = pos[1]
         self.left = self.x - 32
@@ -232,7 +163,6 @@ class LeftBlock:
         self.drawing = True
 
         pass
-
     def update(self):
         pass
 
@@ -240,6 +170,7 @@ class LeftBlock:
         if player.left < self.right and player.right > self.left and player.bottom <= self.top and player.top >= self.bottom:
             if player.left <= self.right:
                 player.x += self.right - player.left
+
         pass
 
     def draw(self):
@@ -255,8 +186,8 @@ class RightBlock:
     image = None
 
     def __init__(self,pos):
-        if RightBlock.image == None:
-            self.image = load_image('chip\\block3.png')
+        if RightBlock.image is None:
+            self.image = load_image('block3.png')
         self.x = pos[0]
         self.y = pos[1]
         self.left = self.x - 32
@@ -290,7 +221,7 @@ class CrushBlock:
     global player
 
     def __init__(self,pos):
-        self.image = load_image('chip\\block3.png')
+        self.image = load_image('block3.png')
         self.x = pos[0]
         self.y = pos[1]
         self.left = self.x - 32
@@ -317,7 +248,7 @@ class CrushBlock:
         if player.left >= self.right and player.right <= self.right + 64 and self.top <= player.y <= self.top + 32:
             if player.state == -3:
                 self.fill = False
-
+        pass
 
     def draw(self):
         if self.fill:
@@ -332,9 +263,11 @@ class CrushBlock:
 class Object:
     global player
     image = None
+
     def __init__(self, pos):
-        if Object.image == None:
-            self.image = load_image('chip\\Apple.png')
+
+        if Object.image is None:
+            self.image = load_image('Apple.png')
         self.x = pos[0]
         self.y = pos[1]
         self.frame = 0
@@ -366,13 +299,71 @@ class Object:
 
 
 def interact():
+    global player, backGround, count
     player.falling = True
     for i in range(0, 33):
         block[i].check_collision()
     for i in crushBlockList:
         i.check_collision()
         i.crush()
-    objectA.check_collision()
+    for i in objectList:
+        i.check_collision()
+
+
+player = Player((500, 500))
+backGround = load_image('realBackGround.png')
+
+for i in range(6):
+    objectList.append(Object((520 + (i * 20),470)))
+
+
+for i in range(0, 10):
+    for k in range(0, 19):
+        if tile_type[i][k] == 1:
+            block.append(DownBlock((32 + 64 * k, 608 - i * 64)))
+            count += 1
+        elif tile_type[i][k] == 2:
+            block.append(LeftBlock((32 + 64 * k, 608 - i * 64)))
+            count += 1
+        elif tile_type[i][k] == 3:
+            crushBlockList.append(CrushBlock((32 + 64 * k, 608 - i * 64)))
+        elif tile_type[i][k] == 4:
+            block.append(RightBlock((32 + 64 * k, 608 - i * 64)))
+            count += 1
+
+
+def handle_events():
+    global player, game
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_KEYDOWN and not player.falling:
+            if event.key == SDLK_RIGHT:
+                player.state = 1
+            elif event.key == SDLK_LEFT:
+                player.state = -1
+            elif event.key == SDLK_UP :
+                player.state = 2
+            elif event.key == SDLK_DOWN :
+                player.state = -2
+            elif event.key == SDLK_x:
+                player.state = 3
+            elif event.key == SDLK_z:
+                player.state = -3
+            elif event.key == SDLK_ESCAPE:
+                game = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_UP or event.key == SDLK_DOWN:
+                player.state = 0
+                pass
+            else:
+                player.state = 0
+        elif event.type == SDL_QUIT:
+            game = False
+    pass
+
+
+
 
 
 
@@ -383,33 +374,28 @@ def interact():
 
 
 
-def draw():
+
+
+
+
+
+
+
+while game:
+    handle_events()
+    player.update()
+    player.update_rect()
+    for i in objectList:
+        i.update()
+    interact()
+
     clear_canvas()
-    backGround.draw(640,320)
+    backGround.draw(640, 320)
     for i in block:
         i.draw()
     for i in crushBlockList:
         i.draw()
+    for i in objectList:
+        i.draw()
     player.draw()
-    objectA.draw()
     update_canvas()
-
-
-def update():
-    player.update()
-    player.update_rect()
-    objectA.update()
-    interact()
-
-# while game:
-#     handle_event()
-#     player.update()
-#     objectA.update()
-#     interact()
-#     clear_canvas()
-#     # stage1.draw()
-#     for i in block:
-#         i.draw()
-#     objectA.draw()
-#     player.drawing()
-#     update_canvas()
--- 20181820192DGP/mygame.py
+++ 20181820192DGP/mygame.py
@@ -1,3 +1,8 @@
 import game_framework
+import pico2d
 
-# fill here
+import stage1_state
+
+pico2d.open_canvas(1280, 640)
+game_framework.run(stage1_state)
+pico2d.close_canvas()
