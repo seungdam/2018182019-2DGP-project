a0b85064f3a965f18f4606b63563e6ca828815df
seungdam(home com) <erfrr@naver.com>
2019-11-23 23:25:30 +0900

    오브젝트 분할

---
+++ 20181820192DGP/apple.py
@@ -0,0 +1,46 @@
+from pico2d import*
+import game_framework
+import new_stage1_state
+
+image_sizeW = 32
+image_sizeH = 32
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+class Apple:
+    global player
+    image = None
+
+    def __init__(self, pos):
+        if Apple.image is None:
+            self.image = load_image('chip\\object\\Apple.png')
+        self.x = pos[0]
+        self.y = pos[1]
+        self.frame = 0
+        self.left = self.x - 10
+        self.top = self.y + 10
+        self.right = self.x + 10
+        self.bottom = self.y - 10
+        self.fill = True
+
+        pass
+    def get_bb(self):
+        return self.x - 10, self.y + 10, self.x + 10, self.y -10
+
+
+    def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 17
+        pass
+
+    def late_update(self):
+        player = new_stage1_state.get_player_info()
+        player.objectNum += 1
+
+    def draw(self):
+        if self.exist:
+            self.image.clip_draw(self.frame * image_sizeW, 0, image_sizeW, image_sizeH, self.x, self.y)
+
+        pass
+
